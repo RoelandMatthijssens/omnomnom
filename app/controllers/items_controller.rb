87545415163db00e26093c655391217608f0d295
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-10-15 21:40:10 +0200

    add items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,45 +1,46 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+	before_action :set_item, only: [:show, :edit, :update, :destroy]
+	respond_to :html, :json, :js
 
-  def index
-    @items = Item.all
-    respond_with(@items)
-  end
+	def index
+		@items = Item.all
+		respond_with(@items)
+	end
 
-  def show
-    respond_with(@item)
-  end
+	def show
+		respond_with(@item)
+	end
 
-  def new
-    @item = Item.new
-    respond_with(@item)
-  end
+	def new
+		@item = Item.new
+		respond_with(@item)
+	end
 
-  def edit
-  end
+	def edit
+	end
 
-  def create
-    @item = Item.new(item_params)
-    @item.save
-    respond_with(@item)
-  end
+	def create
+		@item = Item.new(item_params)
+		@item.save
+		redirect_to :items
+	end
 
-  def update
-    @item.update(item_params)
-    respond_with(@item)
-  end
+	def update
+		@item.update(item_params)
+		respond_with(@item)
+	end
 
-  def destroy
-    @item.destroy
-    respond_with(@item)
-  end
+	def destroy
+		@item.destroy
+		respond_with(@item)
+	end
 
-  private
-    def set_item
-      @item = Item.find(params[:id])
-    end
+	private
+		def set_item
+			@item = Item.find(params[:id])
+		end
 
-    def item_params
-      params.require(:item).permit(:name, :price)
-    end
+		def item_params
+			params.require(:item).permit(:name, :price)
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015190843) do
+ActiveRecord::Schema.define(version: 20141015192535) do
+
+  create_table "items", force: true do |t|
+    t.string   "name"
+    t.float    "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
