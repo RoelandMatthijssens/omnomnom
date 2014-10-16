bc2e4c1512417deddb4d3af9870ecc2cec8a941d
Rik Vanmechelen <rik.vanmechelen@gmail.com>
2014-10-16 22:28:24 +0200

    added navigation links and auth on items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
 	before_action :set_item, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
 	respond_to :html, :json, :js
 
 	def index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def nav_link_to name, path, li_options = {}, link_options = {}
+		if current_page?(path)
+			if !li_options[:class]
+				li_options[:class] = "active"
+			else
+				li_options[:class] += " active"
+			end
+			content_tag(:li, link_to(name, path, link_options), li_options)
+		else
+			content_tag(:li, link_to(name, path, link_options), li_options)
+		end
+	end
 end
