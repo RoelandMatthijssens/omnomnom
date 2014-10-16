3b258b26484ca1a100820bf75e585a404d5ac968
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2014-10-16 22:39:03 +0200

    made into oneliner. not necesarily better though

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
 	def nav_link_to name, path, li_options = {}, link_options = {}
 		if current_page?(path)
-			if !li_options[:class]
-				li_options[:class] = "active"
-			else
-				li_options[:class] += " active"
-			end
+			li_options[:class] = "active "+li_options[:class].to_s
 			content_tag(:li, link_to(name, path, link_options), li_options)
 		else
 			content_tag(:li, link_to(name, path, link_options), li_options)
